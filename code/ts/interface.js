b3351a0ebbb6aac1459840df6ad4ccdac42dd102
chenbowu <admin@younizai.com>
2021-11-02 20:12:12 +0800

    delete

--- code/ts/interface.js
+++
@@ -1,5 +0,0 @@
-function sayHello(name) {
-    console.log("hello " + name);
-}
-sayHello('Pony');
-window.qr = 'qinran';
--- code/ts/interface.ts
+++
@@ -1,9 +0,0 @@
-function sayHello (name:string) {
-    console.log(`hello ${name}`)
-}
-
-sayHello('Pony')
-interface Window {
-    qr: string
-}
-window.qr = 'qinran'
