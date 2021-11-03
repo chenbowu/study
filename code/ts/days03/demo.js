4eaa76690c575e7dec5e759bc331a7a2d851489d
Bowu Chen <1028505817@qq.com>
2021-11-04 00:41:38 +0800

    :white_check_mark:

--- code/ts/days03/demo.js
+++ code/ts/days03/demo.js
@@ -6,8 +6,13 @@
 // }
 // function firstElement<Type> (arr: Type): Type | undefined {
 // }
-function fn(x) {
-    console.log(x);
-    return x;
-}
-fn('Hello World!');
+// function fn(x: string): string {
+//     console.log(x);
+//     return x;
+// } 
+// fn('Hello World!');
+var el = document.getElementById('foo');
+if (!el)
+    throw new Error('找不到id为foo的元素');
+// Type is HTMLElement
+el.innerHTML = 'TypeScript';
